f6f7674b5ff51805fa439c9d9a75e159698fd442
Mevan Samaratunga <mevansam@gmail.com>
2018-02-25 23:15:13 +0400

    first commit

---
+++ pcf/install-pas/pipeline/terraform/gcp/bootstrap_attach.tf
@@ -0,0 +1,72 @@
+#
+# This config file attaches the PCF reference 
+# GCP configuration to the Bootstrap VCP.
+#
+
+# Import Bootstrap state
+data "terraform_remote_state" "bootstrap" {
+  backend = "gcs"
+
+  config {
+    bucket = "appbricks-euw3-tf-states"
+    prefix = "tfacc/cf1-bs"
+  }
+}
+
+#
+# Add Name Servers for ERT zone to bootstrap VPC zone.
+#
+resource "google_dns_record_set" "vpc" {
+  name         = "${google_dns_managed_zone.env_dns_zone.dns_name}"
+  managed_zone = "${data.terraform_remote_state.bootstrap.vpc_dns_zone}"
+
+  type = "NS"
+  ttl  = 300
+
+  rrdatas = [
+    "${google_dns_managed_zone.env_dns_zone.name_servers.0}",
+    "${google_dns_managed_zone.env_dns_zone.name_servers.1}",
+    "${google_dns_managed_zone.env_dns_zone.name_servers.2}",
+    "${google_dns_managed_zone.env_dns_zone.name_servers.3}",
+  ]
+}
+
+#
+# Peer perimeter engineering network to PCF virtual network
+#
+
+resource "google_compute_network_peering" "pcf-engineering" {
+  name         = "${var.prefix}-pcf-engineering"
+  network      = "${google_compute_network.pcf-virt-net.self_link}"
+  peer_network = "${data.terraform_remote_state.bootstrap.engineering_network}"
+}
+
+resource "google_compute_network_peering" "engineering-pcf" {
+  name         = "${var.prefix}-engineering-pcf"
+  network      = "${data.terraform_remote_state.bootstrap.engineering_network}"
+  peer_network = "${google_compute_network.pcf-virt-net.self_link}"
+}
+
+#
+# Allow access to internal resources in PCF virtual network from engineering network
+#
+
+data "google_compute_subnetwork" "engineering" {
+  name   = "${basename(data.terraform_remote_state.bootstrap.engineering_subnetwork)}"
+  region = "${var.gcp_region}"
+}
+
+resource "google_compute_firewall" "engineering-to-pcf-allow-all" {
+  name = "${var.prefix}-engineering-to-pcf-allow-all"
+
+  network = "${google_compute_network.pcf-virt-net.name}"
+
+  allow {
+    protocol = "all"
+  }
+
+  direction = "INGRESS"
+
+  source_ranges = ["${data.google_compute_subnetwork.engineering.ip_cidr_range}"]
+  target_tags   = ["${var.prefix}", "${var.prefix}-opsman"]
+}
---
+++ pcf/install-pas/pipeline/terraform/gcp/dns.tf
@@ -0,0 +1,75 @@
+resource "google_dns_managed_zone" "env_dns_zone" {
+  name        = "${var.prefix}-zone"
+  dns_name    = "${var.pcf_ert_domain}."
+  description = "DNS zone (var.pcf_ert_domain) for the var.prefix deployment"
+}
+
+resource "google_dns_record_set" "ops-manager-dns" {
+  name = "opsman.${google_dns_managed_zone.env_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
+
+  rrdatas = ["${google_compute_instance.ops-manager.network_interface.0.address}"]
+}
+
+resource "google_dns_record_set" "wildcard-sys-dns" {
+  name = "*.${var.system_domain}."
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
+
+  rrdatas = ["${google_compute_global_address.pcf.address}"]
+}
+
+resource "google_dns_record_set" "wildcard-apps-dns" {
+  name = "*.${var.apps_domain}."
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
+
+  rrdatas = ["${google_compute_global_address.pcf.address}"]
+}
+
+resource "google_dns_record_set" "app-ssh-dns" {
+  name = "ssh.${var.system_domain}."
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
+
+  rrdatas = ["${google_compute_address.ssh-and-doppler.address}"]
+}
+
+resource "google_dns_record_set" "doppler-dns" {
+  name = "doppler.${var.system_domain}."
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
+
+  rrdatas = ["${google_compute_address.ssh-and-doppler.address}"]
+}
+
+resource "google_dns_record_set" "loggregator-dns" {
+  name = "loggregator.${var.system_domain}."
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
+
+  rrdatas = ["${google_compute_address.ssh-and-doppler.address}"]
+}
+
+resource "google_dns_record_set" "tcp-dns" {
+  name = "tcp.${google_dns_managed_zone.env_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
+
+  rrdatas = ["${google_compute_address.cf-tcp.address}"]
+}
