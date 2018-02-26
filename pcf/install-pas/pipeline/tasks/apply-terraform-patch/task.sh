a7bce09f8ecb57ea54ac79045ee72cc0f73355e2
Mevan Samaratunga <mevansam@gmail.com>
2018-02-26 14:10:53 +0400

    write google service account credentials to file as that is how the gcs backend expects them

--- pcf/install-pas/pipeline/tasks/apply-terraform-patch/task.sh
+++ pcf/install-pas/pipeline/tasks/apply-terraform-patch/task.sh
@@ -7,3 +7,8 @@ cp -r pcf-pipelines-orig/* pcf-pipelines/
 # templates and attaches PCF VPC it to the bootstrap VPC.
 cp automation-pipelines/pcf/install-pas/pipeline/terraform/$IAAS_TYPE/* \
     pcf-pipelines/install-pcf/$IAAS_TYPE/terraform/
+
+# Save service key to a json file as Terraform GCS 
+# backend only accepts the credential from a file.
+echo "$GCP_SERVICE_ACCOUNT_KEY" \
+    > pcf-pipelines/install-pcf/$IAAS_TYPE/terraform/gcp_service_account_key.json
