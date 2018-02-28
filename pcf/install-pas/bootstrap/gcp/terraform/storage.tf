90443236775b04f27bd2242598104ea384b0997d
Mevan Samaratunga <mevansam@gmail.com>
2018-02-28 14:12:15 +0400

    name change

--- pcf/install-pas/bootstrap/gcp/terraform/storage.tf
+++ pcf/install-pas/bootstrap/gcp/terraform/storage.tf
@@ -2,7 +2,7 @@
 # Create storage bucket for pcf-pipelines terraform state
 #
 resource "google_storage_bucket" "pas-terraform-state" {
-  name          = "${var.vpc_name}-pas-states"
+  name          = "${var.vpc_name}-pas-state"
   location      = "${var.gcp_region}"
   storage_class = "REGIONAL"
 
