b8673a336084c72fadf140a80bd1b395a8d79681
Mevan Samaratunga <mevansam@gmail.com>
2018-02-28 11:15:23 +0400

    fixed pipelines/ to pipeline/

--- pcf/install-pas/pipeline/tasks/add-infrastructure-ext/task.sh
+++ pcf/install-pas/pipeline/tasks/add-infrastructure-ext/task.sh
@@ -6,7 +6,7 @@ cp -r pcf-pipelines-orig/* pcf-pipelines/
 
 # Copy terraform templates that patch the pcf-piplines 
 # templates and attaches PCF VPC it to the bootstrap VPC.
-cp automation-pipelines/pcf/install-pas/pipelines/$IAAS_TYPE/terraform/infrastructure-ext/* \
+cp automation-pipelines/pcf/install-pas/pipeline/$IAAS_TYPE/terraform/infrastructure-ext/* \
     pcf-pipelines/install-pcf/$IAAS_TYPE/terraform/
 
 # Save service key to a json file as Terraform GCS 
