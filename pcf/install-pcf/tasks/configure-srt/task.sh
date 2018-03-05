7fb7242d35767e2d3b026bff4cfbfac046ff6a0e
Mevan Samaratunga <mevansam@gmail.com>
2018-03-05 07:48:49 +0400

    updated ops man cred variables to be consistent with other tasks

--- pcf/install-pcf/tasks/configure-srt/task.sh
+++ pcf/install-pcf/tasks/configure-srt/task.sh
@@ -470,8 +470,10 @@ cf_properties=$(
 
 om-linux \
   --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-  --username "$OPS_MGR_USR" \
-  --password "$OPS_MGR_PWD" \
+  --client-id "${OPSMAN_CLIENT_ID}" \
+  --client-secret "${OPSMAN_CLIENT_SECRET}" \
+  --username "${OPSMAN_USERNAME}" \
+  --password "${OPSMAN_PASSWORD}" \
   --skip-ssl-validation \
   configure-product \
   --product-name cf \
