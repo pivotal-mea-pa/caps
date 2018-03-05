da4df692b132692f4ae2525053dfda9d1ea72d69
Mevan Samaratunga <mevansam@gmail.com>
2018-03-05 22:27:56 +0400

    authorize only access from ert subnet

--- pcf/install-pcf/pipeline/gcp/terraform/sql.tf
+++ pcf/install-pcf/pipeline/gcp/terraform/sql.tf
@@ -15,20 +15,8 @@ resource "google_sql_database_instance" "master" {
 
       authorized_networks = [
         {
-          name  = "nat-1"
-          value = "${google_compute_instance.nat-gateway-pri.network_interface.0.access_config.0.assigned_nat_ip}"
-        },
-        {
-          name  = "nat-2"
-          value = "${google_compute_instance.nat-gateway-sec.network_interface.0.access_config.0.assigned_nat_ip}"
-        },
-        {
-          name  = "nat-3"
-          value = "${google_compute_instance.nat-gateway-ter.network_interface.0.access_config.0.assigned_nat_ip}"
-        },
-        {
-          name  = "all"
-          value = "0.0.0.0/0"
+          name  = "ert"
+          value = "${google_compute_subnetwork.subnet-ert.ip_cidr_range}"
         },
       ]
     }
