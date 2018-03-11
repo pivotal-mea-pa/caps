0eba55631eacd98d1fdd73aa3d7e785ef484297e
Mevan Samaratunga <mevansam@gmail.com>
2018-03-11 20:55:37 +0400

    add ability to replace reference terraform templates with custom set instead of simply patching the reference set

--- pcf/install-and-upgrade/tasks/extend-infrastructure/task.sh
+++ pcf/install-and-upgrade/tasks/extend-infrastructure/task.sh
@@ -4,10 +4,17 @@ set -euo pipefail
 
 cp -r pcf-pipelines-orig/* pcf-pipelines/
 
+patch=${PATCH:-yes}
+tf_template_path=${TF_TEMPLATE_PATH:-automation-pipelines/pcf/install-and-upgrade/pipeline/$IAAS_TYPE/terraform}
+
 # Copy terraform templates that patch the pcf-piplines 
 # templates and attaches PCF VPC it to the bootstrap VPC.
-cp automation-pipelines/pcf/install-and-upgrade/pipeline/$IAAS_TYPE/terraform/* \
-    pcf-pipelines/install-pcf/$IAAS_TYPE/terraform/
+if [[ $patch == yes ]]; then 
+    cp $tf_template_path/* pcf-pipelines/install-pcf/$IAAS_TYPE/terraform/
+else
+    rm -fr pcf-pipelines/install-pcf/$IAAS_TYPE/terraform
+    cp -r $tf_template_path pcf-pipelines/install-pcf/$IAAS_TYPE/terraform
+fi
 
 # Save service key to a json file as Terraform GCS 
 # backend only accepts the credential from a file.
