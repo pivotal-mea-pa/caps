6037f27dd7f8055b956c1c6a9199fc9186f23ae7
Mevan Samaratunga <mevansam@gmail.com>
2018-03-17 12:36:33 +0400

    task to run smoke tests errand for a given set of deployment patterns

---
+++ tasks/bosh/run-smoke-tests/task.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/scripts/iaas-func.sh
+source ~/scripts/bosh-func.sh
+iaas::initialize
+
+[[ -z "$TRACE" ]] || set -x
+set -e
+
+if [[ -e job-session/env ]]; then
+    source job-session/env
+    bosh_client_creds
+fi
+
+bosh::login_client "$CA_CERT" "$BOSH_HOST" "$BOSH_CLIENT" "$BOSH_CLIENT_SECRET"
+bosh::deployment_action run_smoke_tests_errand $DEPLOYMENTS
+
+set +ex
