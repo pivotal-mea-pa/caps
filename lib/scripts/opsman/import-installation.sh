efa06f18fd2bf4efb51cf82ed144d72a25c8bcd9
Mevan Samaratunga <mevansam@gmail.com>
2018-03-26 00:18:00 +0400

    fixed error

--- lib/scripts/opsman/import-installation.sh
+++ lib/scripts/opsman/import-installation.sh
@@ -14,7 +14,7 @@ if [[ ! -e /home/ubuntu/.import_checked ]] &&
   echo "Importing configuration found at '$installation_zip' to new appliance."
   
   set -e
-  curl -k "https://$OPSMAN_URL/api/v0/installation_asset_collection" \
+  echo curl -k "$OPSMAN_URL/api/v0/installation_asset_collection" \
     -X POST \
     -F "installation[file]=@$installation_zip" \
     -F "passphrase=$OPSMAN_ADMIN_PASSWORD"
@@ -25,11 +25,11 @@ if [[ ! -e /home/ubuntu/.import_checked ]] &&
     
     export BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/uaac/Gemfile
 
-    bundle exec uaac target https://$OPSMAN_URL/uaa --skip-ssl-validation > /dev/null 2>&1
+    bundle exec uaac target $OPSMAN_URL/uaa --skip-ssl-validation > /dev/null 2>&1
     while [[ $? -ne 0 ]]; do
       echo "Waiting for Ops Manager UAA to initialize."
       sleep 5
-      bundle exec uaac target https://$OPSMAN_URL/uaa --skip-ssl-validation > /dev/null 2>&1
+      bundle exec uaac target $OPSMAN_URL/uaa --skip-ssl-validation > /dev/null 2>&1
     done
   fi
 fi
