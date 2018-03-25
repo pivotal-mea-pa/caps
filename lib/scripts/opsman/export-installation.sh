a9166b28b173c2aee8958e40677c2ab42c49b2b1
Mevan Samaratunga <mevansam@gmail.com>
2018-03-25 23:00:14 +0400

    added script to export opsman

---
+++ lib/scripts/opsman/export-installation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+OPSMAN_URL="https://${google_dns_record_set.ops-manager-dns.name}/uaa"
+OPSMAN_ADMIN_PASSWORD="${data.terraform_remote_state.bootstrap.opsman_admin_password}"
+
+set -e
+
+export BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/uaac/Gemfile
+bundle exec uaac target $OPSMAN_URL/uaa --skip-ssl-validation
+bundle exec uaac token owner get opsman admin -s "" -p "$OPSMAN_ADMIN_PASSWORD"
+
+TOKEN=$(bundle exec uaac context | awk '/access_token:/{ print $2 }')
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+sudo mkdir -p /data/exports
+sudo chown ubuntu:ubuntu /data/exports
+
+curl -f -k "$OPSMAN_URL/api/v0/installation_asset_collection" \
+  -H "Authorization: Bearer $TOKEN" \
+  -X GET -o /data/exports/installation-$TIMESTAMP.zip
+
+set +e
+
+file /data/exports/installation-$TIMESTAMP.zip | grep 'ASCII text' >/dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+
+  cat /data/exports/installation-$TIMESTAMP.zip
+  echo ''
+  
+  rm /data/exports/installation-$TIMESTAMP.zip
+  exit 1
+fi
+
+sudo chown ubuntu:ubuntu /data/exports/installation-$TIMESTAMP.zip
