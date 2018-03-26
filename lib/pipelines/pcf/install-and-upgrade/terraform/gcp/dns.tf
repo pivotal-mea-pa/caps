62ff573f50fd1d757a026dba6858b94f40432f1a
Mevan Samaratunga <mevansam@gmail.com>
2018-03-26 18:33:35 +0400

    fixed dns name to be of required format

--- lib/pipelines/pcf/install-and-upgrade/terraform/gcp/dns.tf
+++ lib/pipelines/pcf/install-and-upgrade/terraform/gcp/dns.tf
@@ -5,7 +5,7 @@ resource "google_dns_managed_zone" "env_dns_zone" {
 }
 
 resource "google_dns_record_set" "ops-manager-dns" {
-  name = "${local.opsman_dns_name}"
+  name = "opsman.${google_dns_managed_zone.env_dns_zone.dns_name}"
   type = "A"
   ttl  = 300
 
