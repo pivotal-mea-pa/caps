3139b26c49c7aa8a5aff14235b9d0841f68177dc
Mevan Samaratunga <mevansam@gmail.com>
2018-04-11 16:51:07 +0400

    fixed logic error

--- lib/pipelines/pcf/install-and-upgrade/tasks/apply-changes/task.sh
+++ lib/pipelines/pcf/install-and-upgrade/tasks/apply-changes/task.sh
@@ -26,12 +26,15 @@ while [[ true ]]; do
     fi
 
     sleep 10
-    
+
+    time_now=$(date +%s)
+
     ss=$(($time_now-$start_time))
     h=$(($ss/3600))
     m=$(($ss/60-$h*60))
     s=$(($ss-$m*60-$h*3600))
     time_elapsed=$(printf "%02d" $h):$(printf "%02d" $m):$(printf "%02d" $s)
+    
     echo -e -n "Waiting $time_elapsed hours for running installs to complete.\r"
 done
 
