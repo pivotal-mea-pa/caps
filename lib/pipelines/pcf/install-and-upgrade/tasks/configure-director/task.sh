218deae8274dfcf125244554c3175fec6559baf0
Mevan Samaratunga <mevansam@gmail.com>
2018-04-17 18:01:07 +0400

    ignore failure to network assignment changes as it fails if already done as it cannot be changed once applied

--- lib/pipelines/pcf/install-and-upgrade/tasks/configure-director/task.sh
+++ lib/pipelines/pcf/install-and-upgrade/tasks/configure-director/task.sh
@@ -43,7 +43,6 @@ networks_configuration=$(jq -s '.[0] * .[1]' networks-new.json networks-curr.jso
 
 iaas_configuration=$(eval_jq_templates "iaas_configuration" "$TEMPLATE_PATH" "$TEMPLATE_OVERRIDE_PATH" "$IAAS")
 director_configuration=$(eval_jq_templates "director_config" "$TEMPLATE_PATH" "$TEMPLATE_OVERRIDE_PATH" "$IAAS")
-network_assignment=$(eval_jq_templates "network_assignment" "$TEMPLATE_PATH" "$TEMPLATE_OVERRIDE_PATH" "$IAAS")
 security_configuration=$(eval_jq_templates "security_configuration" "$TEMPLATE_PATH" "$TEMPLATE_OVERRIDE_PATH" "$IAAS")
 resource_configuration=$(eval_jq_templates "resource_configuration" "$TEMPLATE_PATH" "$TEMPLATE_OVERRIDE_PATH" "$IAAS")
 
@@ -59,6 +58,23 @@ om \
   --director-configuration "$director_configuration" \
   --az-configuration "$az_configuration" \
   --networks-configuration "$networks_configuration" \
-  --network-assignment "$network_assignment" \
   --security-configuration "$security_configuration" \
   --resource-configuration "$resource_configuration"
+
+set +e
+
+# This will fail if network assignment has already been done. 
+network_assignment=$(eval_jq_templates "network_assignment" "$TEMPLATE_PATH" "$TEMPLATE_OVERRIDE_PATH" "$IAAS")
+
+om \
+  --skip-ssl-validation \
+  --target "https://${OPSMAN_HOST}" \
+  --client-id "${OPSMAN_CLIENT_ID}" \
+  --client-secret "${OPSMAN_CLIENT_SECRET}" \
+  --username "${OPSMAN_USERNAME}" \
+  --password "${OPSMAN_PASSWORD}" \
+  configure-director \
+  --network-assignment "$network_assignment" >/dev/null 2>&1
+
+[[ $? -ne 0 ]] && \
+  echo "Network assignment failed. Most likely this has already been done and cannot be changed once applied."
