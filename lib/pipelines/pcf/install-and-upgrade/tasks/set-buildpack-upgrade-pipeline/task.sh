2d0bcc2ad1950a2c6dd0acd5005c108e679ebe10
Mevan Samaratunga <mevansam@gmail.com>
2018-09-17 22:51:46 +0400

    fixed bash command

--- lib/pipelines/pcf/install-and-upgrade/tasks/set-buildpack-upgrade-pipeline/task.sh
+++ lib/pipelines/pcf/install-and-upgrade/tasks/set-buildpack-upgrade-pipeline/task.sh
@@ -14,15 +14,20 @@ env=$(echo $ENVIRONMENT | awk '{print toupper($0)}')
 echo "\n*** Configuring buildpack upgrade pipeline for ${env} ***\n"
 
 # Setup buildpack upgrade pipeline
-om_cli="om --skip-ssl-validation
-  --target 'https://${OPSMAN_HOST}'
-  --client-id '${OPSMAN_CLIENT_ID}' \
-  --client-secret '${OPSMAN_CLIENT_SECRET}' \
-  --username '${OPSMAN_USERNAME}' \
-  --password '${OPSMAN_PASSWORD}'"
-
-cf_user=$($om_cli credentials -p cf -c .uaa.admin_credentials -f identity)
-cf_password=$($om_cli credentials -p cf -c .uaa.admin_credentials -f password)
+cf_user=$(om --skip-ssl-validation \
+  --target "https://${OPSMAN_HOST}" \
+  --client-id "${OPSMAN_CLIENT_ID}" \
+  --client-secret "${OPSMAN_CLIENT_SECRET}" \
+  --username "${OPSMAN_USERNAME}" \
+  --password "${OPSMAN_PASSWORD}" \
+  credentials -p cf -c .uaa.admin_credentials -f identity)
+cf_password=$(om --skip-ssl-validation \
+  --target "https://${OPSMAN_HOST}" \
+  --client-id "${OPSMAN_CLIENT_ID}" \
+  --client-secret "${OPSMAN_CLIENT_SECRET}" \
+  --username "${OPSMAN_USERNAME}" \
+  --password "${OPSMAN_PASSWORD}" \
+  credentials -p cf -c .uaa.admin_credentials -f password)
 
 cf_api_uri=https://api.$system_domain
 
