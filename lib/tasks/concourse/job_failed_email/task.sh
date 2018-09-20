735c81a3d14dcb9c92391a082fa7a00d0d56c922
Mevan Samaratunga <mevansam@gmail.com>
2018-09-20 19:42:20 +0400

    testing

--- lib/tasks/concourse/job_failed_email/task.sh
+++ lib/tasks/concourse/job_failed_email/task.sh
@@ -6,8 +6,8 @@ env
 fly -t default login -c $CONCOURSE_URL -u ''$CONCOURSE_USER'' -p ''$CONCOURSE_PASSWORD''
 fly -t default sync
 
-build_name=$(fly -t local builds | awk -v j="$PIPELINE_JOB_PATH" '($2 == j){ print $3 }' | sort | tail -1)
-job_output=$(fly -t local watch -j $PIPELINE_JOB_PATH -b $build_name)
+build_name=$(fly -t default builds | awk -v j="$PIPELINE_JOB_PATH" '($2 == j){ print $3 }' | sort | tail -1)
+job_output=$(fly -t default watch -j $PIPELINE_JOB_PATH -b $build_name)
 echo -e "$job_output" | ./ansi2html.sh > email-out/body
 
 cat <<EOF > email-out/headers
