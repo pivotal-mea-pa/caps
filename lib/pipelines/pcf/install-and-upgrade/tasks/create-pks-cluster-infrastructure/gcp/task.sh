15909977992c1424bb0c4d0184281a24404b53f8
Mevan Samaratunga <mevansam@gmail.com>
2018-11-05 23:55:59 +0400

    testing pks cluster infra task

--- lib/pipelines/pcf/install-and-upgrade/tasks/create-pks-cluster-infrastructure/gcp/task.sh
+++ lib/pipelines/pcf/install-and-upgrade/tasks/create-pks-cluster-infrastructure/gcp/task.sh
@@ -4,11 +4,11 @@ source ~/scripts/opsman-func.sh
 source ~/scripts/bosh-func.sh
 root=$PWD
 
-sudo mv pks-release/pks-linux-amd64-* /usr/local/bin/pks
-sudo chmod +x /usr/local/bin/pks
+mv pks-release/pks-linux-amd64-* /usr/local/bin/pks
+chmod +x /usr/local/bin/pks
 
-sudo mv pks-release/kubectl-linux-amd64-* /usr/local/bin/kubectl
-sudo chmod +x /usr/local/bin/kubectl
+mv pks-release/kubectl-linux-amd64-* /usr/local/bin/kubectl
+chmod +x /usr/local/bin/kubectl
 
 [[ -n "$TRACE" ]] && set -x
 set -eu
@@ -16,9 +16,9 @@ set -eu
 # Retrieve bosh credentials from Ops Manager API and set BOSH envrionment
 
 if [[ -n "$OPSMAN_USERNAME" ]]; then
-    opsman::login $OPSMAN_HOST $OPSMAN_USERNAME $OPSMAN_PASSWORD $OPSMAN_DECRYPTION_KEY
+    opsman::login $OPSMAN_HOST $OPSMAN_USERNAME $OPSMAN_PASSWORD
 elif [[ -n "$OPSMAN_CLIENT_ID" ]]; then
-    opsman::login_client $OPSMAN_HOST $OPSMAN_CLIENT_ID $OPSMAN_CLIENT_SECRET $OPSMAN_DECRYPTION_KEY
+    opsman::login_client $OPSMAN_HOST $OPSMAN_CLIENT_ID $OPSMAN_CLIENT_SECRET
 else
     echo "ERROR! Pivotal Operations Manager credentials were not provided."
     exit 1
