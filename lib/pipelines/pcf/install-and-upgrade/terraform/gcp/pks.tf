d75a08b76643f86cb01ed5adc9080aaf5046af53
Mevan Samaratunga <mevansam@gmail.com>
2018-11-06 09:30:22 +0400

    changed role

--- lib/pipelines/pcf/install-and-upgrade/terraform/gcp/pks.tf
+++ lib/pipelines/pcf/install-and-upgrade/terraform/gcp/pks.tf
@@ -68,7 +68,7 @@ resource "google_service_account" "pks-master" {
 
 resource "google_project_iam_binding" "pks-master-iam-computeInstanceAdmin" {
   project = "${data.terraform_remote_state.bootstrap.gcp_project}"
-  role    = "roles/compute.instanceAdmin"
+  role    = "roles/compute.instanceAdmin.v1"
 
   members = [
     "serviceAccount:${google_service_account.pks-master.email}",
