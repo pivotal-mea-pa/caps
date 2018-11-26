068e6be20bc9627d1ad7e47c8d375106e1462abf
Mevan Samaratunga <mevansam@gmail.com>
2018-11-26 23:47:35 +0400

    updated pks-cluster-config task to create load balancers

---
+++ lib/pipelines/pcf/install-and-upgrade/tasks/pks-cluster-config/google/task.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+[[ -n "$TRACE" ]] && set -x
+set -eo pipefail
+
+# Save service key to a json file as Terraform GCS 
+# backend only accepts the credential from a file.
+echo "$GCP_CREDENTIALS" > $root/gcp_service_account_key.json
+
+export GOOGLE_CREDENTIALS=$root/gcp_service_account_key.json
+export GOOGLE_PROJECT=${GCP_PROJECT}
+export GOOGLE_REGION=${GCP_REGION}
+
+terraform init \
+    -backend-config="bucket=${TERRAFORM_STATE_BUCKET}" \
+    -backend-config="prefix=${DEPLOYMENT_PREFIX}-k8s-clusters" \
+    automation/lib/pipelines/pcf/install-and-upgrade/terraform/google/pks-loadbalancers
+
+terraform apply \
+    -auto-approve \
+    -var "infrastructure_state_bucket=${TERRAFORM_STATE_BUCKET}" \
+    -var "infrastructure_state_prefix=${DEPLOYMENT_PREFIX}" \
+    automation/lib/pipelines/pcf/install-and-upgrade/terraform/google/pks-loadbalancers
