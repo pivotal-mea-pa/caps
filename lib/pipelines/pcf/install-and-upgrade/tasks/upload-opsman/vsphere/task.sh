aad07d0d5b671f2a6c57612c1d1e109a97c70d76
Mevan Samaratunga <mevansam@gmail.com>
2019-01-05 23:55:57 +0400

    upload opsman ova as template

--- lib/pipelines/pcf/install-and-upgrade/tasks/upload-opsman/vsphere/task.sh
+++ lib/pipelines/pcf/install-and-upgrade/tasks/upload-opsman/vsphere/task.sh
@@ -3,4 +3,40 @@
 [[ -n "$TRACE" ]] && set -x
 set -eu
 
-exit 1
+download_file_path=$(find ./pivnet-download -name *.tgz | sort | head -1)
+tar xvzf $download_file_path
+
+name=$(basename $download_file_path)
+name=${name%_*}
+
+template_path=/${VCENTER_DATACENTER}/vm/${VCENTER_TEMPLATES_PATH}
+
+ova_file_path=$(find ./pivnet-product -name *.ova | sort | head -1)
+
+set +e
+govc ls ${template_path} | grep ${name} >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  govc folder.create "${template_path}" >/dev/null 2>&1
+
+  govc import.spec \
+    $ova_file_path \
+    | jq \
+    --arg image_name "$name" \
+    --arg network "$OPSMAN_VCENTER_NETWORK" \
+    'del(.Deployment) | del(.PropertyMapping)
+    | .Name = $image_name
+    | .DiskProvisioning = "thin"
+    | .NetworkMapping[].Network = $network
+    | .PowerOn = false
+    | .MarkAsTemplate = true' \
+    > import-spec.json
+
+  govc import.ova \
+    -dc=${VCENTER_DATACENTER} \
+    -ds=${OPSMAN_VCENTER_DATASTORE} \
+    -folder=${template_path} \
+    -options=import-spec.json \
+    $ova_file_path 
+fi
+set -e
+
