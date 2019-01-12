f29d7018e19ef5ee0d84af6ffd3be06b5e26697a
Mevan Samaratunga <mevansam@gmail.com>
2019-01-12 23:53:50 +0400

    quoted search as unquoted string can be problematic

--- lib/pipelines/pcf/install-and-upgrade/terraform/infrastructure/vsphere/get_opsman_image_archive.sh
+++ lib/pipelines/pcf/install-and-upgrade/terraform/infrastructure/vsphere/get_opsman_image_archive.sh
@@ -4,5 +4,5 @@ exec 2> >(tee get_opsman_image_archive.log 2>&1 >/dev/null)
 
 set -exu
 
-opsman_image_archive=$(find ./pivnet-download -name *.tgz | sort | head -1)
+opsman_image_archive=$(find ./pivnet-download -name "*.tgz" | sort | head -1)
 echo "{\"path\":\"$opsman_image_archive\"}"
