c6b6c461f143c6c04f74fbd148fd495293657015
Mevan Samaratunga <mevansam@gmail.com>
2019-01-12 19:34:38 +0400

    testnig

--- lib/pipelines/pcf/install-and-upgrade/tasks/destroy-infrastructure/task.sh
+++ lib/pipelines/pcf/install-and-upgrade/tasks/destroy-infrastructure/task.sh
@@ -65,7 +65,7 @@ case $IAAS in
 
     export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
     export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
-    # export AWS_DEFAULT_REGION=$S3_DEFAULT_REGION
+    export AWS_DEFAULT_REGION=$S3_DEFAULT_REGION
     
     # Default is to use an S3 backend
     if [[ -n $TF_STATE_S3_ENDPOINT ]]; then
@@ -92,12 +92,12 @@ esac
 # Delete infrastructure
 echo "Deleting provisioned infrastructure..."
 
-backend_type=$(cat .terraform/terraform.tfstate | jq -r .backend.type)
-cat << ---EOF > backend.tf
-terraform {
-  backend "$backend_type" {}
-}
----EOF
+# backend_type=$(cat .terraform/terraform.tfstate | jq -r .backend.type)
+# cat << ---EOF > backend.tf
+# terraform {
+#   backend "$backend_type" {}
+# }
+# ---EOF
 
 set +e
 
