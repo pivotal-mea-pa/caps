5e0ff835d76903d2adc52a970e32dc7b7b10f63b
Mevan Samaratunga <mevansam@gmail.com>
2019-01-13 19:26:51 +0400

    return empty response if downloads folder does not exist

--- lib/pipelines/pcf/install-and-upgrade/terraform/infrastructure/google/scripts/get_opsman_image_archive.sh
+++ lib/pipelines/pcf/install-and-upgrade/terraform/infrastructure/google/scripts/get_opsman_image_archive.sh
@@ -4,13 +4,18 @@ exec 2> >(tee get_opsman_image_archive.log 2>&1 >/dev/null)
 
 set -exu
 
-opsman_image_archive=$(find ./pivnet-download -name "*.tgz" | sort | head -1)
-tar xzf $opsman_image_archive
+if [[ -e ./pivnet-download ]]; then
 
-# us: ops-manager-us/pcf-gcp-1.9.2.tar.gz -> ops-manager-us/pcf-gcp-1.9.2.tar.gz
-opsman_bucket_path=$(grep -i 'us:.*.tar.gz' pivnet-product/*GCP.yml | cut -d' ' -f2)
+  opsman_image_archive=$(find ./pivnet-download -name "*.tgz" | sort | head -1)
+  tar xzf $opsman_image_archive
 
-# ops-manager-us/pcf-gcp-1.9.2.tar.gz -> opsman-pcf-gcp-1-9-2
-opsman_image_name=$(echo $opsman_bucket_path | sed 's%.*/\(.*\).tar.gz%opsman-\1%' | sed 's/\./-/g')
+  # us: ops-manager-us/pcf-gcp-1.9.2.tar.gz -> ops-manager-us/pcf-gcp-1.9.2.tar.gz
+  opsman_bucket_path=$(grep -i 'us:.*.tar.gz' pivnet-product/*GCP.yml | cut -d' ' -f2)
 
-echo "{\"image_name\":\"$opsman_image_name\",\"bucket_path\":\"$opsman_bucket_path\"}"
+  # ops-manager-us/pcf-gcp-1.9.2.tar.gz -> opsman-pcf-gcp-1-9-2
+  opsman_image_name=$(echo $opsman_bucket_path | sed 's%.*/\(.*\).tar.gz%opsman-\1%' | sed 's/\./-/g')
+
+  echo "{\"image_name\":\"$opsman_image_name\",\"bucket_path\":\"$opsman_bucket_path\"}"
+else
+  echo "{\"image_name\":\"\",\"bucket_path\":\"\"}"
+fi
