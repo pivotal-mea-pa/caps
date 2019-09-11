dd7d2e86c9f787e66db003fe3facee0b6d30da99
Mevan Samaratunga <mevansam@gmail.com>
2019-09-11 23:59:54 +0400

    remove harbor patch as it is working now

--- lib/pipelines/pcf/install-and-upgrade/tasks/pks-runtime-config/task.sh
+++ lib/pipelines/pcf/install-and-upgrade/tasks/pks-runtime-config/task.sh
@@ -45,61 +45,61 @@ export BOSH_CLIENT_SECRET=$(opsman::get_director_client_secret ops_manager)
 bosh::login_client "$BOSH_CA_CERT" "$BOSH_ENVIRONMENT" "$BOSH_CLIENT" "$BOSH_CLIENT_SECRET"
 
 # Retrieve opsman certificate
-OPSMAN_CA_CERT=$(om \
-  --skip-ssl-validation \
-  --target "https://${OPSMAN_HOST}" \
-  --client-id "${OPSMAN_CLIENT_ID}" \
-  --client-secret "${OPSMAN_CLIENT_SECRET}" \
-  --username "${OPSMAN_USERNAME}" \
-  --password "${OPSMAN_PASSWORD}" \
-  certificate-authorities \
-  --format json \
-  | jq -r '.[] | select(.issuer | match("Pivotal")) | .cert_pem')
+# OPSMAN_CA_CERT=$(om \
+#   --skip-ssl-validation \
+#   --target "https://${OPSMAN_HOST}" \
+#   --client-id "${OPSMAN_CLIENT_ID}" \
+#   --client-secret "${OPSMAN_CLIENT_SECRET}" \
+#   --username "${OPSMAN_USERNAME}" \
+#   --password "${OPSMAN_PASSWORD}" \
+#   certificate-authorities \
+#   --format json \
+#   | jq -r '.[] | select(.issuer | match("Pivotal")) | .cert_pem')
 
 # Create Bosh runtime-config to patch Harbor as it does not
 # correctly configure the self-signed CA root certificate
 # for validation when using self-signed certificates for UAA.
-harbor_deployment=$(bosh::deployment harbor-*)
-if [[ -n $harbor_deployment ]]; then
+# harbor_deployment=$(bosh::deployment harbor-*)
+# if [[ -n $harbor_deployment ]]; then
 
-  latest_release_version=$(bosh releases | awk '/^patch-harbor/{ print $2 }' | head -1)
-  release_version=${latest_release_version%\**}
+#   latest_release_version=$(bosh releases | awk '/^patch-harbor/{ print $2 }' | head -1)
+#   release_version=${latest_release_version%\**}
 
-  bosh \
-    --deployment=${harbor_deployment} \
-    manifest \
-    > $harbor_deployment.yml
+#   bosh \
+#     --deployment=${harbor_deployment} \
+#     manifest \
+#     > $harbor_deployment.yml
 
-  cat << ---EOF > ${harbor_deployment}_runtime-config.yml
----
-releases:
-- name: patch-harbor
-  version: $release_version
+#   cat << ---EOF > ${harbor_deployment}_runtime-config.yml
+# ---
+# releases:
+# - name: patch-harbor
+#   version: $release_version
 
-addons:
-- name: patch-harbor
-  jobs:
-  - name: patch-harbor
-    release: patch-harbor
-  properties:
-    opsman_ca_cert: |
-$(echo -e "$OPSMAN_CA_CERT" | sed 's|^|      |g')
-  include:
-    deployments:
-    - $harbor_deployment
----EOF
+# addons:
+# - name: patch-harbor
+#   jobs:
+#   - name: patch-harbor
+#     release: patch-harbor
+#   properties:
+#     opsman_ca_cert: |
+# $(echo -e "$OPSMAN_CA_CERT" | sed 's|^|      |g')
+#   include:
+#     deployments:
+#     - $harbor_deployment
+# ---EOF
 
-  bosh --non-interactive \
-    update-config \
-    --name=patch_${harbor_deployment} \
-    --type=runtime \
-    ${harbor_deployment}_runtime-config.yml
+#   bosh --non-interactive \
+#     update-config \
+#     --name=patch_${harbor_deployment} \
+#     --type=runtime \
+#     ${harbor_deployment}_runtime-config.yml
 
-  bosh --non-interactive \
-    --deployment=${harbor_deployment} \
-    deploy \
-    $harbor_deployment.yml
-fi
+#   bosh --non-interactive \
+#     --deployment=${harbor_deployment} \
+#     deploy \
+#     $harbor_deployment.yml
+# fi
 
 # Apply Terraform templates to configure PKS users
 # and create initial clusters.
