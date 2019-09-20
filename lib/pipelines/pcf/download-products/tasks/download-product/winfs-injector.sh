272a1ed5555debe83baf1db7cbbbf3c08312cb18
hodeh <hodeh@pivotal.io>
2019-09-20 16:52:24 +0400

    tar inside pivnet-product

--- lib/pipelines/pcf/download-products/tasks/download-product/winfs-injector.sh
+++ lib/pipelines/pcf/download-products/tasks/download-product/winfs-injector.sh
@@ -15,10 +15,11 @@ echo "PATH TO TAR $PATH_TO_TAR"
 tar -xf $PATH_TO_TAR -C injector-workspace
 
 ##COPY STEMCELL
+mkdir pivnet-download-updated/pivnet-product
 PATH_TO_STEMCELL=`find ./injector-workspace/pivnet-product/ -name *go_agent.tgz`
 echo $PATH_TO_STEMCELL
 STEMCELL_FILENAME=`basename $PATH_TO_STEMCELL`
-cp $PATH_TO_STEMCELL pivnet-download-updated/$STEMCELL_FILENAME
+mv $PATH_TO_STEMCELL pivnet-download-updated/pivnet-product/$STEMCELL_FILENAME
 
 PATH_TO_INJECTOR_ZIP=`find ./injector-workspace/pivnet-product/ -name winfs-injector-*`
 echo "PATH TO ZIP $PATH_TO_INJECTOR_ZIP"
@@ -29,7 +30,8 @@ chmod +x injector-workspace/pivnet-product/winfs-injector-linux
 PATH_TO_TILE=`find ./injector-workspace/pivnet-product/ -name *.pivotal`
 echo "PATH TO tile $PATH_TO_TILE"
 
-./injector-workspace/pivnet-product/winfs-injector-linux --input-tile=$PATH_TO_TILE --output-tile=pivnet-download-updated/injected-tile.pivotal
-tar cvzf ./pivnet-download-updated/pivnet-product-stemcell.tgz ./pivnet-download-updated/injected-tile.pivotal ./pivnet-download-updated/$STEMCELL_FILENAME
-rm -rf pivnet-download-updated/injected-tile.pivotal
-rm -rf pivnet-download-updated/$STEMCELL_FILENAME
+./injector-workspace/pivnet-product/winfs-injector-linux --input-tile=$PATH_TO_TILE --output-tile=pivnet-download-updated/pivnet-product/injected-tile.pivotal
+cd pivnet-download-updated
+tar cvzf pivnet-product-stemcell.tgz ./pivnet-product/injected-tile.pivotal ./pivnet-product/$STEMCELL_FILENAME
+rm -rf ./pivnet-product/injected-tile.pivotal
+rm -rf ./pivnet-product/$STEMCELL_FILENAME
